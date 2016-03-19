15a39eca8397711036b1a109805906b0b2e1792b
Sebasu <Sebasu_Knutti@hotmail.com>
2016-03-19 19:20:09 -0400

    v1.2.2
    Android 6.0 support
    New auth method

---
+++ src/main/java/com/vagoscorp/virtualterminal/prokey/MainActivity.java
@@ -0,0 +1,44 @@
+package com.vagoscorp.virtualterminal.prokey;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    TextView stat;
+    Button enPro;
+
+    Intent appIntent;
+    Intent appMIntent;
+
+    public static final String PRO = "PRO";
+    public static final int isPRO = 2016;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        PackageManager manager = getPackageManager();
+        setContentView(R.layout.activity_main);
+        stat = (TextView) findViewById(R.id.state);
+        enPro = (Button) findViewById(R.id.enPro);
+        appIntent = manager.getLaunchIntentForPackage("com.vagoscorp.virtualterminal");
+        appMIntent = new Intent(Intent.ACTION_VIEW);
+        appMIntent.setData(Uri.parse("market://details?id=com.vagoscorp.virtualterminal"));
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (appIntent != null) {
+            appIntent.putExtra(PRO, isPRO);
+            startActivity(appIntent);
+            finish();
+        } else
+            startActivity(appMIntent);
+        super.onDestroy();
+    }
+}
